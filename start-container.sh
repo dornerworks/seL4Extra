d91384b64218d6f3e5023bc07d972879dc2f0cf2
Jesse Millwood <jesse.millwood@dornerworks.com>
2019-11-21 15:49:45 -0500

    [Container] Added support for paths with spaces in start-container

--- start-container.sh
+++ start-container.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 DOCKER_PATH="./tools/DockerFiles"
+pwddir="$(pwd)"
+host_dir="${pwddir// /\\ }"
 
-make -C ${DOCKER_PATH} user_sel4-riscv USER_IMG=user_img-$(shell whoami)-riscv HOST_DIR=$(pwd)
+make -C ${DOCKER_PATH} user_sel4-riscv USER_IMG=user_img-$(shell whoami)-riscv HOST_DIR="${host_dir}"
