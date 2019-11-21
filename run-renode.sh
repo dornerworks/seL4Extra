71d0d11feac04bbb5221ee7ffcb1aa2b5f12c65d
Jesse Millwood <jesse.millwood@dornerworks.com>
2019-11-21 15:51:25 -0500

    [Renode] Added support for paths with spaces in run-renode

--- run-renode.sh
+++ run-renode.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 renode_bin=renode
-scripts_path="/home/jesse/DornerWorksProjects/RISC-V\ Summit\ 2019/Code/Repo-sel4test/tools/sel4Extra/renode_support"
-resc_path="${scripts_path// /\ }"
-echo "Scripts : ${scripts_path}"
+scripts_path="$(pwd)"
+resc_path="${scripts_path// /\\ }"
+
 echo "RescPath: ${resc_path}"
 ${renode_bin} "${resc_path}/sel4test.resc"
