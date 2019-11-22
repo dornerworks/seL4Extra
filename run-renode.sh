063963a05c69f79b58059a059c9bc87de21d962c
Jesse Millwood <jesse.millwood@dornerworks.com>
2019-11-22 14:28:34 -0500

    [Renode] Support if dtb isn't created yet

--- run-renode.sh
+++ run-renode.sh
@@ -3,6 +3,14 @@
 renode_bin=renode
 scripts_path="$(pwd)/tools/sel4Extra/renode_support"
 resc_path="${scripts_path// /\\ }"
+dtb_path="${resc_path}/hifive-renode.dtb"
 
-echo "RescPath: ${resc_path}"
+echo "RESC Script: ${resc_path}"
+if [ -f "${dtb_path}" ]; then
+    echo " DTB Exsists"
+else
+    echo "Making DTB"
+    $(cd ${resc_path} && ./create-dtb.sh || echo "DTB creation failed, exiting.. " && exit 1)
+fi
+
 ${renode_bin} "${resc_path}/sel4test.resc"
