6f6639462f80f47fcf778ee75df9cc0bffca770c
Jesse Millwood <jesse.millwood@dornerworks.com>
2019-12-09 17:56:42 -0500

    [Container] Change docker make target called by start-container

--- start-container.sh
+++ start-container.sh
@@ -4,4 +4,4 @@ DOCKER_PATH="./tools/DockerFiles"
 pwddir="$(pwd)"
 host_dir="${pwddir// /\\ }"
 
-make -C ${DOCKER_PATH} user_sel4-riscv USER_IMG=user_img-$(shell whoami)-riscv HOST_DIR="${host_dir}"
+make -C ${DOCKER_PATH} user_camkes-riscv USER_IMG=user_img-$(shell whoami)-riscv HOST_DIR="${host_dir}"
